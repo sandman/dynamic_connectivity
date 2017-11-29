2ec28d632dfe0e487e455d4231d78a4bc6b9d72c
Sandip Gangakhedkar <sandipfloyd@gmail.com>
2017-11-29 14:56:48 +0100

    Added positions to nodes
    Positions are non-overlapping and aware of node arrivals/departures

--- generate_topology.py
+++ generate_topology.py
@@ -10,11 +10,18 @@ random.seed(datetime.now())
 
 # Garage related parameters
 floors = 4
-capacityPerFloor = 50*np.ones(floors)
+capacityPerFloor = 40  # Only supports same capacity for all floors
+capacityOfFloors = capacityPerFloor*np.ones(floors)
 coverageProbFloor = [0.9, 0.8, 0.7, 0.2]
-inCoverageCapacity = capacityPerFloor * coverageProbFloor
+inCoverageCapacity = capacityOfFloors * coverageProbFloor
 occupancy = np.zeros(floors)
 
+#positions in the garage
+x_pos = [5*(i+1) for i in range(10)]
+y_pos = [3, 14, 19, 30]
+z_pos = [2, 6, 10, 14]
+positions = [[x,y,z] for z in z_pos for y in y_pos for x in x_pos]
+availPositions = positions
 # Probability of Relay capable UEs among universe
 relayProb = 0.3
 
@@ -35,7 +42,7 @@ class Vehicle:
         self.remainingTime =  round(scale_par*np.random.weibull(shape_par))
         self.batteryLevel = 10
         self.relayCap = (0 if random.random() > relayProb else 1)
-        self.position = [0,0]
+        self.position = [0,0,0]
         self.floor = 0
         self.connectedList = []
 
@@ -85,7 +92,7 @@ for t in range(0,simTime,1):
         #New arrival
         print "Iter: %d New Arrival(s): %d vehicle(s)" % (t , arrivalTimes.count(t))
         for vehi in range(1, arrivalTimes.count(t)+1):
-            if np.any(np.greater(capacityPerFloor,occupancy)): #Available parking in garage
+            if np.any(np.greater(capacityOfFloors,occupancy)): #Available parking in garage
                 v = Vehicle()
                 #print "Park duration: %d" % v.remainingTime
                 tmp = np.greater_equal(occupancy, inCoverageCapacity)
@@ -94,6 +101,13 @@ for t in range(0,simTime,1):
                     # Assign a floor to UE based on prioritized list of floors (this will change for coverage optimized deployment)
                     #print "Adding vehicle out-of-coverage"
                     v.floor = np.nonzero(tmp)[0][0]
+                    # idx = capacityPerFloor*v.floor + np.random.randint(capacityPerFloor)
+                    # v.position = availPositions[idx]
+
+                    # Randomly select an available position from the selected floor
+                    v.position = random.choice([item for item in availPositions if item[2] == z_pos[v.floor]])
+                    availPositions.remove(v.position) #Remove the selected position from the list of available positions
+
                     #v.active = True
                     # TODO: Update v.connectedList according to graph status (ex. radio distance..)
                     G.add_node(v)
@@ -103,6 +117,8 @@ for t in range(0,simTime,1):
                 else:   #UE in coverage of BS..
                     #print "Adding vehicle in-coverage"
                     v.floor = np.nonzero(tmp==0)[0][0]
+                    v.position = random.choice([item for item in availPositions if item[2] == z_pos[v.floor]])
+                    availPositions.remove(v.position) #Remove the selected position from the list of available positions
                     v.active = True
                     # TODO: Update v.connectedList according to graph status (ex. radio distance..)
                     G.add_node(v)
@@ -120,6 +136,7 @@ for t in range(0,simTime,1):
             if n.remainingTime <= 0:
                 toBeRemoved.add_node(n)
                 occupancy[n.floor] = occupancy[n.floor] - 1
+                availPositions.append(n.position)  # Add the position back to the list of available positions
                 #print "Removing vehicle %d" % n.id
 
     #print "Before deletion: %d" % len(G)
@@ -139,7 +156,7 @@ plt.xlabel('Time (mins)')
 
 plt.figure(2)
 plt.grid()
-plt.bar([1, 2 ,3 ,4],np.divide(occupancy,capacityPerFloor))
+plt.bar([1, 2 ,3 ,4],np.divide(occupancy,capacityOfFloors))
 plt.xlabel('Floor')
 plt.ylabel('Occupancy Ratio')
 plt.ylim([0 , 1])
